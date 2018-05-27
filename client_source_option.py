096dfa59a044b068f5f81516e6b9ed3a054c7a59
Karthik <karthik.bhata7@gmail.com>
2018-05-27 15:34:05 +0530

    Server updated for passive commands

---
+++ client_source_option.py
@@ -0,0 +1,408 @@
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+from scapy.all import *
+from random import randint
+from threading import Thread
+from time import sleep
+import sys
+from Queue import Queue
+import argparse
+import netifaces as ni
+cmd_passive = ['LIST', 'RETR', 'STOR']
+cmd_sender = ['STOR']
+
+class FTPListener:
+	'''	
+		Initializes fields
+	'''
+	def __init__(self, src, dst, sport, dport):
+
+		self.sport = sport
+		self.dport = dport
+		self.src = src
+		self.dst = dst
+		self.next_seq = 1000
+		self.next_ack = 1
+		self.tcp_flags = {
+			'TCP_FIN': 0x01, 
+			'TCP_SYN': 0x02, 
+			'TCP_RST': 0x04, 
+			'TCP_PSH': 0x08, 
+			'TCP_ACK': 0x10, 
+			'TCP_URG': 0x20, 
+			'TCP_ECE': 0x40, 
+			'TCP_CWR': 0x80
+		}
+		self.verbose = False
+		self.passive_port = None
+		self.data_share = Queue([50000]) 
+
+	'''
+		sniff_filter: filters packet based on port it is listening on
+	'''
+	def sniff_filter(self, pkt):
+		return pkt.haslayer(IP) and pkt[IP].src==self.dst and pkt[IP].dst == self.src and pkt.haslayer(TCP) and pkt[TCP].dport == self.sport and pkt[TCP].sport == self.dport
+
+	'''
+		manage_resp: Based on recieved packet, constructs the reply
+	'''
+	def manage_resp(self, pkt):
+		if Raw in pkt:
+			print pkt[Raw].load
+			self.data_share.put(pkt[Raw].load)
+
+		if Raw in pkt and pkt[Raw].load[0:3] == '227':
+			ip_port = pkt[Raw].load.split('(')[1].split(')')[0].split(',')
+			port = int(ip_port[-2]) * 256 + int(ip_port[-1])
+			self.passive_port = port
+
+		self.next_seq = pkt[TCP].ack
+		next_ack = self.get_next_ack(pkt)
+
+		if (pkt[TCP].flags == self.tcp_flags['TCP_ACK']):
+			if next_ack != 1:
+				self.send_ack(pkt)
+		else:
+			self.send_ack(pkt)
+
+
+	'''
+		get_next_ack: Update the next_ack based on segment length of recieved packet.
+	'''
+	def get_next_ack(self, pkt):
+		total_len = pkt.getlayer(IP).len
+		ip_hdr_len = pkt.getlayer(IP).ihl * 32 / 8
+		tcp_hdr_len = pkt.getlayer(TCP).dataofs * 32 / 8
+		ans = total_len - ip_hdr_len - tcp_hdr_len
+		return (ans if ans else 1)
+
+	'''
+		listen: sniffs packets and filters based on helper functions
+	'''
+	def listen(self):
+		sniff(lfilter=self.sniff_filter, prn=self.manage_resp, stop_filter=self.finish)
+
+	'''
+		send_ack: Acknowledges the recieved packet
+	'''
+	def send_ack(self, pkt):
+		ip = IP(src=self.src, dst=self.dst)
+		self.next_ack = pkt[TCP].seq + self.get_next_ack(pkt)
+		ack = TCP(sport=self.sport, dport=self.dport, flags='A', seq=self.next_seq, ack=self.next_ack)
+		send(ip/ack, verbose=self.verbose)
+
+	'''
+		finish: end of sniffing condition
+	'''
+	def finish(self, pkt):
+		if pkt.haslayer(TCP) and (pkt[TCP].flags & self.tcp_flags['TCP_FIN']):
+			self.next_seq = pkt[TCP].ack
+			self.send_ack(pkt)
+			return True
+		return False
+
+	'''
+		get_passive_port: Returns passive port it is listening on
+	'''
+	def get_passive_port(self):
+		return self.passive_port
+
+
+class FTPPassive:
+	'''	
+		Initializes fields
+	'''
+	def __init__(self, src, dst, sport, dport):
+		self.sport = sport
+		self.dport = dport
+		self.src = src
+		self.dst = dst
+		self.basic_pkt = IP(src=self.src, dst=self.dst)/TCP(sport=self.sport, dport=self.dport)
+		self.tcp_flags = {
+			'TCP_FIN': 0x01, 
+			'TCP_SYN': 0x02, 
+			'TCP_RST': 0x04, 
+			'TCP_PSH': 0x08, 
+			'TCP_ACK': 0x10, 
+			'TCP_URG': 0x20, 
+			'TCP_ECE': 0x40, 
+			'TCP_CWR': 0x80
+		}
+		self.verbose = False
+		self.listener = FTPListener(self.src, self.dst, self.sport, self.dport)
+		self.listen_thread = Thread(target = self.listener.listen)
+		self.listen_thread.start()
+
+	'''
+		send_syn: sends SYN packet.
+	'''
+	def send_syn(self):
+		ip = IP(src=self.src, dst=self.dst)
+		syn = TCP(sport=self.sport, dport=self.dport, flags='S', seq=self.listener.next_seq, ack=self.listener.next_ack)
+		pkt = ip/syn
+		seq_next = self.listener.next_seq 
+		while self.listener.next_seq == seq_next:
+			send(pkt, verbose=self.verbose)
+			sleep(1)
+		return
+
+	'''
+		handshake: Initialize handshake
+	'''
+	def handshake(self):
+		self.send_syn()
+
+	'''
+		get_next_ack: Update the next_ack based on segment length of recieved packet.
+	'''
+	def get_next_ack(self, pkt):
+		total_len = pkt.getlayer(IP).len
+		ip_hdr_len = pkt.getlayer(IP).ihl * 32 / 8
+		tcp_hdr_len = pkt.getlayer(TCP).dataofs * 32 / 8
+		ans = total_len - ip_hdr_len - tcp_hdr_len
+		return (ans if ans else 1)
+
+
+	'''
+		close: closes the TCP connection. Issue found. To be fixed 
+	'''
+	def close(self):
+		self.listen_thread.join()
+		pkt = self.basic_pkt
+		pkt[TCP].flags = 'FA'
+		pkt[TCP].seq = self.listener.next_seq
+		pkt[TCP].ack = self.listener.next_ack
+		sr1(pkt, verbose=self.verbose)
+
+	'''
+		send_pkt: Puts packet on the wire
+	'''
+	def send_pkt(self, pkt):
+		seq_next = self.listener.next_seq 
+		while self.listener.next_seq == seq_next:
+			send(pkt, verbose=self.verbose)
+			sleep(1)
+		return
+
+	'''
+		recvfile: saves the file recieved (RETR)
+	'''
+	def recvfile(self, file):
+		with open(file, "w") as f:
+			while True:
+				if not self.listener.data_share.empty():
+					f.write(self.listener.data_share.get())
+				if self.listener.data_share.empty() and not self.listen_thread.isAlive():
+					break
+
+	'''
+		sendfile: Reads file to be sent (STOR) and sends it to server
+	'''
+	def sendfile(self, file):
+		with open(file, "r") as f:
+			data = f.read()
+			chunks = [data[i:i+512] for i in range(0, len(data), 512)]
+		for item in chunks:
+			pkt = self.basic_pkt
+			pkt[TCP].flags = 'AP'
+			pkt[TCP].seq = self.listener.next_seq
+			pkt[TCP].ack = self.listener.next_ack
+			pkt = pkt/Raw(load=item)
+			self.send_pkt(pkt)
+
+
+class FTPClient:
+	'''	
+		Initializes fields
+	'''
+	def __init__(self, src, dst, sport, dport):
+		self.sport = sport
+		self.dport = dport
+		self.src = src
+		self.dst = dst
+		self.basic_pkt = IP(src=self.src, dst=self.dst)/TCP(sport=self.sport, dport=self.dport)
+		self.tcp_flags = {
+			'TCP_FIN': 0x01, 
+			'TCP_SYN': 0x02, 
+			'TCP_RST': 0x04, 
+			'TCP_PSH': 0x08, 
+			'TCP_ACK': 0x10, 
+			'TCP_URG': 0x20, 
+			'TCP_ECE': 0x40, 
+			'TCP_CWR': 0x80
+		}
+		self.passive_port = None
+		self.verbose = False
+		self.listener = FTPListener(self.src, self.dst, self.sport, self.dport)
+		self.listen_thread = Thread(target = self.listener.listen)
+		self.listen_thread.start()
+		self.passive_obj = None
+
+	'''
+		send_syn: sends SYN packet.
+	'''
+	def send_syn(self):
+		ip = IP(src=self.src, dst=self.dst)
+		syn = TCP(sport=self.sport, dport=self.dport, flags='S', seq=self.listener.next_seq, ack=self.listener.next_ack)
+		pkt = ip/syn
+		seq_next = self.listener.next_seq 
+		while self.listener.next_seq == seq_next:
+			print pkt.summary()
+			send(pkt, verbose=self.verbose)
+			sleep(1)
+		return
+
+	'''
+		send_pkt: Puts packet on the wire
+	'''
+	def send_pkt(self, pkt):
+		seq_next = self.listener.next_seq 
+		while self.listener.next_seq == seq_next:
+			send(pkt, verbose=self.verbose)
+			sleep(1)
+		return
+
+	'''
+		handshake: Initialize handshake
+	'''
+	def handshake(self):
+		self.send_syn()
+
+
+	'''
+		get_next_ack: Update the next_ack based on segment length of recieved packet.
+	'''
+	def get_next_ack(self, pkt):
+		total_len = pkt.getlayer(IP).len
+		ip_hdr_len = pkt.getlayer(IP).ihl * 32 / 8
+		tcp_hdr_len = pkt.getlayer(TCP).dataofs * 32 / 8
+		ans = total_len - ip_hdr_len - tcp_hdr_len
+		return (ans if ans else 1)
+
+	'''
+		passive: send PASV command and initialize passive port
+	'''
+	def passive(self):
+		pkt = self.basic_pkt
+		pkt[TCP].flags = 'AP'
+		pkt[TCP].seq = self.listener.next_seq
+		pkt[TCP].ack = self.listener.next_ack
+		cmd = "PASV\r\n"
+		pasv = pkt/cmd
+		self.send_pkt(pasv)
+		self.passive_port = self.listener.get_passive_port()
+
+	'''
+		manage_passive: if new passive connection is created, respective helper functions called.		
+	'''
+	def manage_passive(self, command):
+		sport = random.randint(1024, 65535)
+		passive = FTPPassive(self.src, self.dst, sport, self.passive_port)
+		passive.handshake()
+		base_cmd = command.split('\r\n')[0]
+		cmd = base_cmd.split(' ')[0]
+		if cmd == 'STOR':
+			passive.sendfile(base_cmd.split(' ')[1])
+		if cmd == 'RETR':
+			recv_thread = Thread(target=passive.recvfile, args=(base_cmd.split(' ')[1], ))
+			recv_thread.start()
+		return passive
+
+	'''
+		close: closes the TCP connection. Issues found while closing. Need to be fixed
+	'''
+	def close(self):
+		pkt = self.basic_pkt
+		pkt[TCP].flags = 'FA'
+		pkt[TCP].seq = self.listener.next_seq
+		pkt[TCP].ack = self.listener.next_ack
+		send(pkt, verbose=self.verbose)
+		self.listen_thread.join()
+
+	'''
+		run_command: sends the command and based on command, if it runs on passive mode, it creates an FTPPassive object.
+	'''
+	def run_command(self, command):
+		if command == 'exit\r\n':
+			self.close()
+			return
+		base_cmd = command.split('\r')[0].split(' ')[0]
+		if base_cmd in cmd_passive:
+			self.passive()
+			self.passive_obj = self.manage_passive(command) 
+			pkt = self.basic_pkt
+			pkt[TCP].flags = 'AP'
+			pkt[TCP].seq = self.listener.next_seq
+			pkt[TCP].ack = self.listener.next_ack
+			cmd = pkt/command
+			self.send_pkt(cmd)
+			self.passive_obj.close()
+		else:
+			pkt = self.basic_pkt
+			pkt[TCP].flags = 'AP'
+			pkt[TCP].seq = self.listener.next_seq
+			pkt[TCP].ack = self.listener.next_ack
+			cmd = pkt/command
+			self.send_pkt(cmd)
+
+	'''
+		interactive: Takes input from user and sends it to server through run_command	
+	'''
+	def interactive(self):
+		command = ""
+		while True:
+			sys.stdout.write(">> ")
+			command = raw_input() + '\r\n'
+			if command == 'exit\r\n':
+				self.close()
+				return
+			else:
+				self.run_command(command)
+
+if __name__ == '__main__':
+
+	source_ip = None
+	source_port = None
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-6', '--ipv6', help='User FTP on IPv6 network', action='store_true')
+	parser.add_argument('-u', '--user', help='Username for FTP login', nargs=1, type=str, required=True)
+	parser.add_argument('-l', '--passwd', help='Password for FTP login', nargs=1, type=str, required=True)
+	parser.add_argument('-i', '--ipaddr', help='FTP server IP address', nargs=1, type=str, required=True)
+	parser.add_argument('-p', '--port', help='Port address of FTP server', nargs=1, type=int, required=True)
+	parser.add_argument('-si', '--source_ip', help='client ip address', nargs=1, type=str, required=False)
+	parser.add_argument('-sp', '--source_port', help='client port', nargs=1, type=int, required=False)
+	parser.add_argument('--iface', help='Interface of which the client should put the packets', nargs=1, type=str, required=False)
+	args = parser.parse_args()
+
+	if args.ipv6:
+		IP = IPv6
+
+	if not args.source_ip and not args.iface:
+		print 'Either specify source ip and port or give interface to be used'
+		sys.exit(0)
+
+	if args.source_ip:
+		if not args.source_port:
+			print 'Source port not found'
+			sys.exit(0)
+		source_ip = args.source_ip[0]
+		source_port = args.source_port[0]
+	else:
+		try:
+			source_ip = ni.ifaddresses(args.iface[0])[ni.AF_INET][0]['addr']
+			source_port = random.randint(1024, 65535)
+		except:
+			print 'Invalid interface name', args.iface[0]
+			sys.exit(0)
+
+
+	dest_ip = args.ipaddr[0]
+	dest_port = args.port[0]
+
+	client = FTPClient(source_ip, dest_ip, source_port, dest_port)
+	client.handshake()
+	client.run_command('USER ' + args.user[0] + '\r\n')
+	client.run_command('PASS ' + args.passwd[0] + '\r\n')
+	client.interactive()
--- server.py
+++ server.py
@@ -12,6 +12,164 @@ from Queue import Queue
 import argparse
 
 
+local_ip = socket.gethostbyname(socket.gethostname())
+
+class FTP_Passive_Listener:
+	# Initializes the fields
+	def __init__(self, src, dst, sport):
+		self.src = src
+		self.dst = dst
+		self.sport = sport
+		self.dport = None
+		self.data_share = Queue([100])
+		self.next_seq = 0
+		self.next_ack = None
+		self.tcp_flags = {
+			'TCP_FIN': 0x01,
+			'TCP_SYN': 0x02,
+			'TCP_RST': 0x04,
+			'TCP_PSH': 0x08,
+			'TCP_ACK': 0x10,
+			'TCP_URG': 0x20,
+			'TCP_ECE': 0x40,
+			'TCP_CWR': 0x80
+		}
+		self.basic_pkt = IP(src=self.src, dst=self.dst)/TCP(sport=self.sport)
+		self.verbose = False
+		
+
+
+	# gets next ack number based on TCP segment length
+	def get_next_ack(self, pkt):
+		total_len = pkt.getlayer(IP).len
+		ip_hdr_len = pkt.getlayer(IP).ihl * 32 / 8
+		tcp_hdr_len = pkt.getlayer(TCP).dataofs * 32 / 8
+		ans = total_len - ip_hdr_len - tcp_hdr_len
+		return (ans if ans else 1)
+
+	# Sends ack packet
+	def send_ack(self, pkt):
+		self.next_ack = pkt[TCP].seq + self.get_next_ack(pkt)
+		ack = TCP(sport=self.sport, dport=self.dport, flags='A', seq=self.next_seq, ack=self.next_ack)
+		send(self.basic_pkt/ack, verbose=self.verbose)
+
+	# filters packet based on port and ip. To get packet directed to current connection
+	def sniff_filter(self, pkt):
+		if pkt.haslayer(IP) and pkt[IP].src == self.dst and pkt[IP].dst == self.src \
+			and pkt.haslayer(TCP) and pkt[TCP].dport == self.sport:
+			return True
+		return False
+
+	# stopping condition for sniffing
+	def finish(self, pkt):
+		if pkt.haslayer(TCP) and (pkt[TCP].flags & self.tcp_flags['TCP_FIN']):
+			self.next_seq = pkt[TCP].ack
+			self.send_ack(pkt)
+			# need FIN?
+			return True
+		return False
+
+	# manages recieved packet and replies accordingly
+	def manage_resp(self, pkt):
+
+		if pkt[TCP].flags & self.tcp_flags['TCP_SYN']:
+			self.next_ack = pkt[TCP].seq + 1			
+			dst = pkt[IP].src
+			src = pkt[IP].dst
+			self.dport = pkt[TCP].sport
+			self.basic_pkt[TCP].dport = self.dport 
+			ackno = pkt[TCP].seq + 1
+			seqno = 0 # use random
+			synack = IP(src=src, dst=dst)/TCP(sport=self.sport, dport=self.dport, flags='SA', seq=seqno, ack=ackno)
+			reply = None
+			while not reply:
+				reply = sr1(synack, timeout=1, verbose=self.verbose)
+			seqno += 1
+
+		if pkt[TCP].flags & self.tcp_flags['TCP_ACK']:
+			self.next_seq = pkt[TCP].ack
+		if Raw in pkt:
+			self.data_share.put(pkt[Raw].load)
+
+
+	# Starts listening on the wire
+	def run(self):
+		sniff(prn=self.manage_resp, lfilter=self.sniff_filter, stop_filter=self.finish)
+
+	def command(self, command , cwd):
+		self.__currdir = cwd
+		cmd = command.split(' ')[0]
+		if cmd == "LIST":
+		      k = ""
+	 	      print 'list:', self.__currdir
+  	      	      for t in os.listdir(self.__currdir):
+  	          	k+=self.toListItem(os.path.join(self.__currdir,t)) + '\n'
+  	      	      self.send_data(k+'\r\n')                    
+		      self.close()
+		      return
+		
+		if cmd == "STOR":
+			with open(command.split(' ')[1], "w") as f:
+				while True:
+					if not self.data_share.empty():
+						f.write(self.data_share.get())
+					if self.data_share.empty():
+						break
+			self.close()
+
+		if cmd == "RETR" :
+			with open(command.split(' ')[1],"r") as f:
+				data = f.read()
+				chunks = [data[i:i+512] for i in range(0, len(data), 512)]
+			for item in chunks:
+				pkt = self.basic_pkt
+				pkt[TCP].flags = 'AP'
+				pkt[TCP].seq = self.next_seq
+				pkt[TCP].ack = self.next_ack
+				pkt = pkt/Raw(load=item)
+				print pkt
+				self.send_pkt(pkt)
+			self.close()
+
+		else:
+		      return
+
+
+	def close(self):
+		pkt = self.basic_pkt
+		pkt[TCP].flags = 'FA'
+		pkt[TCP].seq = self.next_seq
+		pkt[TCP].ack = self.next_ack
+		sr1(pkt, verbose=self.verbose)	
+
+
+
+
+      	def toListItem(self,fn):
+    	    st=os.stat(fn)
+    	    fullmode='rwxrwxrwx'
+    	    mode=''
+    	    for i in range(9):
+    	        mode+=((st.st_mode>>(8-i))&1) and fullmode[i] or '-'
+    	    d=(os.path.isdir(fn)) and 'd' or '-'
+    	    ftime=time.strftime(' %b %d %H:%M ', time.gmtime(st.st_mtime))
+    	    return d+mode+' 1 user group '+str(st.st_size)+ftime+os.path.basename(fn)
+			
+	# Put the reply into a packet
+	def send_data(self, data):
+		pkt = self.basic_pkt/Raw(load=data)
+		self.send_pkt(pkt)
+
+	# send the packet
+	def send_pkt(self, pkt):
+		pkt[TCP].flags = 'PA'
+		pkt[TCP].seq = self.next_seq
+		pkt[TCP].ack = self.next_ack
+		while self.next_seq == pkt[TCP].seq:
+			sr1(pkt, timeout=1, verbose=self.verbose)
+				
+
+
 class FTPListener:
 	# Initializes the fields
 	def __init__(self, src, dst, sport, dport, seqno, ackno):
@@ -23,17 +181,18 @@ class FTPListener:
 		self.next_seq = seqno
 		self.next_ack = ackno
 		self.tcp_flags = {
-			'TCP_FIN': 0x01, 
-			'TCP_SYN': 0x02, 
-			'TCP_RST': 0x04, 
-			'TCP_PSH': 0x08, 
-			'TCP_ACK': 0x10, 
-			'TCP_URG': 0x20, 
-			'TCP_ECE': 0x40, 
+			'TCP_FIN': 0x01,
+			'TCP_SYN': 0x02,
+			'TCP_RST': 0x04,
+			'TCP_PSH': 0x08,
+			'TCP_ACK': 0x10,
+			'TCP_URG': 0x20,
+			'TCP_ECE': 0x40,
 			'TCP_CWR': 0x80
 		}
 		self.basic_pkt = IP(src=self.src, dst=self.dst)/TCP(sport=self.sport, dport=self.dport)
 		self.verbose = False
+
 
 	# gets next ack number based on TCP segment length
 	def get_next_ack(self, pkt):
@@ -84,13 +243,17 @@ class FTPListener:
 	def run(self):
 		sniff(prn=self.manage_resp, lfilter=self.sniff_filter, stop_filter=self.finish)
 
+
+
+
 class FTPServerConnectiton:
 
 	__dirname = path.abspath(".")
 	__currdir = __dirname
 	__pasv = False
 	__finish = False
-
+	__rest = False
+	__rename_ready = False
 	# FTP responses
 	__resp = {
 	    'welcome': '220 Welcome!\r\n',
@@ -105,15 +268,25 @@ class FTPServerConnectiton:
 	    'curr_dir': '257 "%s" is the current directory.\r\n',
 	    'not_file': '550 %s: not a regular file.\r\n',
 	    'dir_changed': '250 Directory changed.\r\n',
+	    'specify_filename':'421 Specify Filename.\r\n',
 	    'dir_changed_root': '250 Directory changed to root.\r\n',
 	    'path_not_found': '550 Path not found.Directory not changed\r\n',
+	    'file_not_found': '550 file_not_found in Directory \r\n', 	
 	    'path_not_dir': '550 Path is not a directory.Directory not changed\r\n',
 	    'use_pasv': '425 Use PASV or PORT first.\r\n',
 	    'open_data_conn': '150 Opening data connection.\r\n',
 	    'transfer_complete': '226 Transfer complete.\r\n',
 	    'dir_list': '150 Here comes the directory listing.\r\n',
-		'pasv_mode': '227 Entering Passive Mode (%s,%u,%u).\r\n',
-	}
+	    'pasv_mode': '227 Entering Passive Mode (%s,%u,%u).\r\n',
+	    'dir_send':'226 Directory send OK.\r\n',
+	    'dir_create':'257 Directory created.\r\n',
+	    'dir_delete':'250 Directory deleted.\r\n',
+	    'not_allowed':'450 Not allowed.\r\n',
+	    'file_delete':'250 File deleted.\r\n',
+	    'rename_from':'350 Ready.\r\n',
+	    'rename_to':'250 File Renamed.\r\n',
+
+  	}
 
 	# initialize the fields
 	def __init__(self, src, dst, sport, dport, seqno, ackno):
@@ -121,14 +294,16 @@ class FTPServerConnectiton:
 		self.dst = dst
 		self.sport = sport
 		self.dport = dport
+		self.s = seqno
+		self.a = ackno
 		self.tcp_flags = {
-			'TCP_FIN': 0x01, 
-			'TCP_SYN': 0x02, 
-			'TCP_RST': 0x04, 
-			'TCP_PSH': 0x08, 
-			'TCP_ACK': 0x10, 
-			'TCP_URG': 0x20, 
-			'TCP_ECE': 0x40, 
+			'TCP_FIN': 0x01,
+			'TCP_SYN': 0x02,
+			'TCP_RST': 0x04,
+			'TCP_PSH': 0x08,
+			'TCP_ACK': 0x10,
+			'TCP_URG': 0x20,
+			'TCP_ECE': 0x40,
 			'TCP_CWR': 0x80
 		}
 		self.basic_pkt = IP(src=self.src, dst=self.dst)/TCP(sport=self.sport, dport=self.dport)
@@ -138,6 +313,7 @@ class FTPServerConnectiton:
 		self.listener_thread.start()
 
 	# Run the commands
+
 	def command(self, cmd):
 		print 'command %s recieved' % (cmd,)
 
@@ -149,7 +325,7 @@ class FTPServerConnectiton:
 
 	# Functions for respective commands
 
-	def USER(self, cmd):
+	def USER(self,cmd):
 		try:
 			self.user = cmd.split(' ')[1]
 		except:
@@ -157,7 +333,7 @@ class FTPServerConnectiton:
 			return
 		self.send_data(self.__resp['username_ok'])
 
-	def PASS(self, cmd):
+	def PASS(self,cmd):
 		try:
 			self.passwd = cmd.split(' ')[1]
 		except:
@@ -165,6 +341,136 @@ class FTPServerConnectiton:
 			return
 		self.send_data(self.__resp['password_ok'])
 
+
+        def LIST(self,cmd):
+	      self.send_data(self.__resp['dir_list'])	     
+	      self.passive_listener.command(cmd,self.__currdir)
+	      self.passive_listener_thread.join()
+	      self.send_data(self.__resp['dir_send'])
+	      return
+
+	def STOR(self,cmd):
+	     self.send_data(self.__resp['open_data_conn'])
+	     self.passive_listener.command(cmd,self.__currdir)	
+ 	     self.passive_listener_thread.join()
+	     self.send_data(self.__resp['transfer_complete'])
+	     return	
+
+	def RETR(self,cmd):
+	     self.send_data(self.__resp['open_data_conn'])
+	     self.passive_listener.command(cmd,self.__currdir)	
+ 	     self.passive_listener_thread.join()
+	     self.send_data(self.__resp['transfer_complete'])
+	     return
+
+	def MKD(self,cmd):
+        	try:
+		 	dn=os.path.join(cmd.split(' ')[1])
+        		os.mkdir(dn)
+		except:
+			self.send_data(self.__resp['not_allowed'])
+			return
+		self.send_data(self.__resp['dir_create'])
+		return
+
+	def RMD(self,cmd):
+		if os.path.isdir(cmd.split(' ')[1]):
+			os.rmdir(fn)
+			self.send_data(self.__resp['dir_delete'])
+        	else:
+			self.send_data(self.__resp['not_allowed'])
+			return
+
+	def DELE(self,cmd):
+		fn=os.path.join(cmd.split(' ')[1])
+		if os.path.exists(cmd.split(' ')[1]):
+			os.remove(fn)
+			self.send_data(self.__resp['file_delete'])
+        	else:
+			self.send_data(self.__resp['not_allowed'])
+			return
+
+	def CDUP(self,cmd):
+		try:
+			if not os.path.samefile(self.__currdir,self.__dirname):
+				self.__currdir=os.path.abspath(os.path.join(self.__currdir,'..'))
+			self.send_data(self.__resp['dir_changed_root'])
+			return
+		except:
+			self.send_data(self.__resp['not_allowed'])
+			return
+
+	def PWD(self,cmd):
+        	cwd=os.path.relpath(self.__currdir,self.__dirname)
+       		if cwd=='.':
+        	   	cwd='/'
+		    	self.send_data('257 \"%s\"\r\n' % cwd)
+		    	return
+       		else:
+            		cwd='/'+cwd
+		    	self.send_data('257 \"%s\"\r\n' % cwd)
+		    	return
+		return
+
+	def CWD(self,cmd):
+        	 chwd=os.path.join(cmd.split(' ')[1])
+	   	 if chwd=='/':
+        		self.__currdir=self.__dirname
+       		 elif chwd[0]=='/':
+			if(os.path.isdir(os.path.join(self.__dirname,chwd[1:]))):
+        			self.__currdir=os.path.join(self.__dirname,chwd[1:])
+				self.send_data(self.__resp['dir_changed'])
+				return
+			else:
+         			self.send_data(self.__resp['path_not_found'])
+				return
+            	 else:
+		  	if(os.path.isdir(os.path.join(self.__currdir,chwd))):
+ 	       			self.__currdir=os.path.join(self.__currdir,chwd)
+				self.send_data(self.__resp['dir_changed'])
+				return
+			else:
+				self.send_data(self.__resp['path_not_found'])
+				return
+        def RNFR(self,cmd):
+		try:    	    	
+			self.rnfn=os.path.join(self.__currdir,cmd.split(' ')[1])
+			print self.rnfn
+			if os.path.exists(self.rnfn):       	
+				self.send_data(self.__resp['rename_from'])
+				self.__rename_from = True
+			else:
+				self.send_data(self.__resp['file_not_found'])	
+		except:
+			self.send_data(self.__resp['specify_filename'])	
+			return
+
+    	def RNTO(self,cmd):
+		try:		
+			if self.__rename_from:
+        			fn=os.path.join(self.__currdir,cmd.split(' ')[1])
+        			os.rename(self.rnfn,fn)
+        			self.send_data(self.__resp['rename_to'])
+				self.__rename_from = False
+			else:
+				self.send_data(self.__resp['not_allowed'])
+		except:
+			self.send_data(self.__resp['specify_filename'])	
+		return
+	
+    	def PASV(self,cmd):
+        	self.__pasv = True
+		ip = self.src
+        	port = 20
+        	print 'open', ip, port
+		self.send_data('227 Entering Passive Mode (%s,%u,%u).\r\n' %
+        	        (','.join(ip.split('.')), port>>8&0xFF, port&0xFF))
+
+   		self.passive_listener = FTP_Passive_Listener(self.src, self.dst, port)
+		self.passive_listener_thread = Thread(target=self.passive_listener.run)
+		self.passive_listener_thread.start()
+
+		
 
 	def SYST(self, cmd):
 		self.send_data(self.__resp['syst_msg'])
@@ -191,20 +497,19 @@ class FTPServerConnectiton:
 				self.command(cmd)
 
 
-
 class FTPServer:
 	# initialize the fields
 	def __init__(self, sport):
 		self.sport = sport
 		self.verbose = False
 		self.tcp_flags = {
-			'TCP_FIN': 0x01, 
-			'TCP_SYN': 0x02, 
-			'TCP_RST': 0x04, 
-			'TCP_PSH': 0x08, 
-			'TCP_ACK': 0x10, 
-			'TCP_URG': 0x20, 
-			'TCP_ECE': 0x40, 
+			'TCP_FIN': 0x01,
+			'TCP_SYN': 0x02,
+			'TCP_RST': 0x04,
+			'TCP_PSH': 0x08,
+			'TCP_ACK': 0x10,
+			'TCP_URG': 0x20,
+			'TCP_ECE': 0x40,
 			'TCP_CWR': 0x80
 		}
 
@@ -228,7 +533,7 @@ class FTPServer:
 
 	# Filter packets to be recieved based on port and flag (SYN)
 	def sniff_filter(self, pkt):
-		return pkt.haslayer(TCP) and pkt[TCP].dport == self.sport and pkt[TCP].flags == self.tcp_flags['TCP_SYN'] 
+		return pkt.haslayer(TCP) and pkt[TCP].dport == self.sport and pkt[TCP].flags == self.tcp_flags['TCP_SYN']
 		# and pkt.haslayer(IP) and pkt[IP].dst == self.src
 
 	def run(self):
